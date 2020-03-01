5b1654cadf795ad9bc66ef2d69a27a9c5c3ea253
Tomi <tomica.korpar@gmail.com>
2020-03-01 19:28:39 +0100

    organize imports

--- src/main/java/com/tomicakorpar/eventapp/repository/DogadajRepository.java
+++ src/main/java/com/tomicakorpar/eventapp/repository/DogadajRepository.java
@@ -1,13 +1,9 @@
 package com.tomicakorpar.eventapp.repository;
 
-import com.tomicakorpar.eventapp.dto.DogadajDto;
 import com.tomicakorpar.eventapp.entity.Dogadaj;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface DogadajRepository extends JpaRepository<Dogadaj, Integer>, QuerydslPredicateExecutor<Dogadaj> {
